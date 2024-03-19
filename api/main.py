51424a31297e67e263154308ae2f826e9a1bf1e2
Benhur Essissongo <105126658+Jbenhur8901@users.noreply.github.com>
2024-03-19 11:24:42 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,24 @@
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+from api.tool import retrieval
+
+app = FastAPI()
+
+class Parameters(BaseModel):
+    index : str
+    query : str
+
+@app.get("/")
+async def health_check():
+    return {"status":"ok"}
+
+@app.post('/inference')
+async def get_response(parameters : Parameters):
+    args = parameters.model_dump()
+    respone = retrieval(args["index"], args["query"])
+    return {"inference": respone}
+
+if __name__ == "__main__":
+    uvicorn.run(app)
 
